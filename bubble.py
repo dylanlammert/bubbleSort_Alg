e4829a9d2dbd8641d841fe4210667266b679a203
Dylan Lammert <Dylanjlammert@gmail.com>
2025-01-02 20:13:50 -0600

    created bubble sort function, formatted the print statements so it is easier to read.

--- bubble.py
+++ bubble.py
@@ -1,12 +1,23 @@
 import random
-'''
+
+###########
+
 def bubble_alg(array):
-    for i in range(len(array)):
-        for j in range(0, ):
-            if array
-'''
-
-random_numbers = [random.randint(1,100) for x in range (10)]
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+            
+###########
+# size of the array
+n = 10
+# generate n number of random numbers for the array
+random_numbers = [random.randint(1,100) for x in range (n)]
+print("\n")
+print("Unsorted " + str(n) + " numbers")
 print(random_numbers)
-
+print("\n")
+bubble_alg(random_numbers)
+print("Sorted " + str(n) + " numbers")
+print(random_numbers)
 
