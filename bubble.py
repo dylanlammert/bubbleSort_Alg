626b518d639aec8b5e3975a7e3b7a3f918f8683f
Dylan Lammert <Dylanjlammert@gmail.com>
2025-01-02 18:45:08 -0600

    made the file and created the array for the random numbers

---
+++ bubble.py
@@ -0,0 +1,12 @@
+import random
+'''
+def bubble_alg(array):
+    for i in range(len(array)):
+        for j in range(0, ):
+            if array
+'''
+
+random_numbers = [random.randint(1,100) for x in range (10)]
+print(random_numbers)
+
+
