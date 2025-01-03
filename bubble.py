f80b985f6bd72641cad4cf2ef9b0cf3a6e67091e
Dylan Lammert <Dylanjlammert@gmail.com>
2025-01-02 20:18:50 -0600

    added comments

--- bubble.py
+++ bubble.py
@@ -1,14 +1,13 @@
 import random
 
-###########
 
+# bubble sort function
 def bubble_alg(array):
     for i in range(n):
         for j in range(0, n - i - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
             
-###########
 # size of the array
 n = 10
 # generate n number of random numbers for the array
