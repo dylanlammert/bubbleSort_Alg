a76d33cb504f7ca33c3a585d1f595a9947ddc1e9
Dylan Lammert <Dylanjlammert@gmail.com>
2025-01-05 13:14:03 -0600

    imported matplotlib to give a visual representation of the numbers being sorted

--- bubble.py
+++ bubble.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import random
 
 
@@ -5,11 +6,18 @@ import random
 def bubble_alg(array):
     for i in range(n):
         for j in range(0, n - i - 1):
+            # placing values on a bar chart
+            plt.bar(plotvalues, random_numbers)
+            plt.pause(.000001)
+            plt.clf()
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
             
 # size of the array
-n = 10
+n = 50
+plotvalues = list(range(1, n + 1))
+# print(plotvalues)
+    
 # generate n number of random numbers for the array
 random_numbers = [random.randint(1,100) for x in range (n)]
 print("\n")
